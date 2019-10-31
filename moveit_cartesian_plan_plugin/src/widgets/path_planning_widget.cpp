e4ec419bd1c716332a83f7da22b642c6ddb5682a
Mrunal <mrunaljsarvaiya@gmail.com>
2019-10-30 17:11:04 -0700

    Fix tf naming in add nav pose

--- moveit_cartesian_plan_plugin/src/widgets/path_planning_widget.cpp
+++ moveit_cartesian_plan_plugin/src/widgets/path_planning_widget.cpp
@@ -842,12 +842,12 @@ void PathPlanningWidget::addNavPoseHelper()
 
   // Transforms 
   tf2_ros::TransformListener tfListener(tfBuffer_);
-  geometry_msgs::Transform robot_world_tf;
-  geometry_msgs::Transform object_world_tf;
+  geometry_msgs::Transform map_mobilebaselink_tf;
+  geometry_msgs::Transform map_object_tf;
 
-  Eigen::Affine3d object_world_eigen;
-  Eigen::Affine3d robot_object_eigen;
-  Eigen::Affine3d robot_world_eigen;
+  Eigen::Affine3d map_object_eigen;
+  Eigen::Affine3d object_mobilebaselink_eigen;
+  Eigen::Affine3d map_mobilebaselink_eigen;
 
   geometry_msgs::Quaternion quat_msg;
 
@@ -860,8 +860,8 @@ void PathPlanningWidget::addNavPoseHelper()
   if (get_objects_proxy_.call(srv)){
     for(auto& obj : srv.response.objects){
       if(obj.id == object_id){
-        object_world_tf = obj.origin;
-        tf::transformMsgToEigen (object_world_tf, object_world_eigen);
+        map_object_tf = obj.origin;
+        tf::transformMsgToEigen (map_object_tf, map_object_eigen);
         found_tf = true;
         break;
       }
@@ -881,8 +881,8 @@ void PathPlanningWidget::addNavPoseHelper()
   int count = 0;
   while(true){
     try{
-      robot_world_tf = tfBuffer_.lookupTransform("map", "mobile_base_link", ros::Time(0)).transform;
-      tf::transformMsgToEigen (robot_world_tf, robot_world_eigen);
+      map_mobilebaselink_tf = tfBuffer_.lookupTransform("map", "mobile_base_link", ros::Time(0)).transform;
+      tf::transformMsgToEigen (map_mobilebaselink_tf, map_mobilebaselink_eigen);
       break;
     }
     catch (tf2::TransformException &ex/*tf::TransformException ex*/) {
@@ -896,7 +896,7 @@ void PathPlanningWidget::addNavPoseHelper()
   }
 
   // Robot wrt object 
-  robot_object_eigen = object_world_eigen.inverse() * robot_world_eigen;
+  object_mobilebaselink_eigen = map_object_eigen.inverse() * map_mobilebaselink_eigen;
 
   // Get clean path
   peanut_cotyledon::CleanPath clean_path;
@@ -917,16 +917,16 @@ void PathPlanningWidget::addNavPoseHelper()
   }
 
   // Convert Transform to pose and update cached path
-  Eigen::Matrix3d rot = robot_object_eigen.linear();
+  Eigen::Matrix3d rot = object_mobilebaselink_eigen.linear();
   Eigen::Quaterniond quat(rot);
   tf::quaternionEigenToMsg(quat, quat_msg);
 
-  geometry_msgs::Pose robot_object_pose;
-  robot_object_pose.position.x = robot_object_eigen.translation()[0];
-  robot_object_pose.position.y = robot_object_eigen.translation()[1];
-  robot_object_pose.position.z = robot_object_eigen.translation()[2];
-  robot_object_pose.orientation = quat_msg;
-  clean_path.cached_paths.at(0).nav_pose = robot_object_pose;
+  geometry_msgs::Pose object_mobilebaselink_pose;
+  object_mobilebaselink_pose.position.x = object_mobilebaselink_eigen.translation()[0];
+  object_mobilebaselink_pose.position.y = object_mobilebaselink_eigen.translation()[1];
+  object_mobilebaselink_pose.position.z = object_mobilebaselink_eigen.translation()[2];
+  object_mobilebaselink_pose.orientation = quat_msg;
+  clean_path.cached_paths.at(0).nav_pose = object_mobilebaselink_pose;
 
   // Set clean path
   peanut_cotyledon::SetCleanPath set_path_srv;
